3badb926e0ea5e15bc62392e2618e72c98c4d051
Lance C Simons <lancecsimons@gmail.com>
2018-01-28 14:23:22 -0800

    Post-init joystick initialization

--- src/player.cpp
+++ src/player.cpp
@@ -31,26 +31,47 @@ void Player::update()
         right = glm::normalize(mouseDiff);
 
         auto & lmb = client.input.mouseState[SDL_BUTTON_LEFT];
-        
+
         input.firing = lmb == SDL_PRESSED;
+
+        // Allow for post-init controller detection. 
+        if (!client.controller)
+        {
+            int numJoysticks = SDL_NumJoysticks();
+
+            //Check for joysticks
+            if( numJoysticks >= 1 )
+            {
+                for(int i = 0; i < numJoysticks; i++)
+                {
+                    if (SDL_IsGameController(i))
+                    {
+                        printf("Using controller index %i, name %s\n", i, SDL_GameControllerNameForIndex(i));
+                        client.controller = SDL_GameControllerOpen(i);
+                        client.joystick = SDL_GameControllerGetJoystick(client.controller);
+                        break;
+                    }
+                }
+            }
+        }
 
         // Overwrite with controller input if we aren't detecting anything
         if (glm::length(left) < 0.1f && client.controller)
         {
             left.x = glm::clamp(
-                (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_LEFTX) /  32767.0f, 
+                (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_LEFTX) /  32767.0f,
                 -1.0f, 1.0f);
             left.y = glm::clamp(
-                (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_LEFTY) /  32767.0f, 
+                (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_LEFTY) /  32767.0f,
                 -1.0f, 1.0f);
 
             if (!input.firing)
             {
                 right.x = glm::clamp(
-                    (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_RIGHTX) /  32767.0f, 
+                    (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_RIGHTX) /  32767.0f,
                     -1.0f, 1.0f);
                 right.y = glm::clamp(
-                    (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_RIGHTY) /  32767.0f, 
+                    (float)SDL_GameControllerGetAxis(client.controller, SDL_CONTROLLER_AXIS_RIGHTY) /  32767.0f,
                     -1.0f, 1.0f);
 
                 input.firing = glm::length(input.aim) > 0.2f;
@@ -105,7 +126,7 @@ void Player::update()
                 botInput.move = glm::diskRand(1.0f);
             }
 
-            
+
             botInput.firing = glm::linearRand(0.0f, 1.0f) > 0.5f;
 
             botWait = glm::linearRand(botMin, botMax);
@@ -140,12 +161,12 @@ void Player::update()
 
     // if (isHuman)
     // {
-    //     printf("input move: %.2f, %.2f\tvelocity: %.2f, %.2f\tpos: %.2f, %.2f, heading: %.2f\n", 
+    //     printf("input move: %.2f, %.2f\tvelocity: %.2f, %.2f\tpos: %.2f, %.2f, heading: %.2f\n",
     //         input.move.x, input.move.y,
     //         vel.x, vel.y,
     //         pos.x, pos.y,
     //         input.heading);
     // }
 
-    
+
 }
